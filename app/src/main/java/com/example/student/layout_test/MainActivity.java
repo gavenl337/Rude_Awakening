79c187d396951123440339d01f4531fde8c237af
student <student@ssu-cs-dar28-167-216.sonoma.edu>
2019-04-11 12:15:12 -0700

    start to bomb game, button added to home screen for now until alarm is running

--- app/src/main/java/com/example/student/layout_test/MainActivity.java
+++ app/src/main/java/com/example/student/layout_test/MainActivity.java
@@ -6,11 +6,14 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private Button gameButton;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -36,9 +39,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gameButton = findViewById(R.id.button_to_game);
+
+        gameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3 = new Intent(MainActivity.this, bomb.class);
+                startActivity(intent3);
+            }
+        });
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
     }
 
 }
---
+++ app/src/main/java/com/example/student/layout_test/bomb.java
@@ -0,0 +1,13 @@
+package com.example.student.layout_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class bomb extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bomb);
+    }
+}
