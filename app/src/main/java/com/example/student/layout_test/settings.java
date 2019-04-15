d645825c2b168d22b7b85f238c80b0c7b4f62acf
student <student@ssu-cs-dar25-167-138.sonoma.edu>
2019-04-15 13:01:24 -0700

    Settings page working on text size and color. Building and running

--- app/src/main/java/com/example/student/layout_test/settings.java
+++ app/src/main/java/com/example/student/layout_test/settings.java
@@ -7,6 +7,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.graphics.Color;
 
 public class settings extends AppCompatActivity {
 
