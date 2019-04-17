bda12522cd3f30566f621ccc40275099ac566869
student <student@ssu-cs-dar28-167-217.sonoma.edu>
2019-04-17 12:05:35 -0700

    Toggle buttons only let one be selectedgit add .git add .

--- app/src/main/java/com/example/student/layout_test/settings.java
+++ app/src/main/java/com/example/student/layout_test/settings.java
@@ -7,11 +7,18 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.graphics.Color;
+import android.widget.ToggleButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.CompoundButton;
+
 
 public class settings extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private ToggleButton easy;
+    private ToggleButton medium;
+    ToggleButton hard;
+    ToggleButton extraHard;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -38,77 +45,44 @@ public class settings extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        easy = (ToggleButton) findViewById(R.id.difficultyEasy);
+        medium = (ToggleButton) findViewById(R.id.difficultyMedium);
+        hard = (ToggleButton) findViewById(R.id.difficultyHard);
+        extraHard = (ToggleButton) findViewById(R.id.difficultyExtraHard);
+
+        easy.setOnCheckedChangeListener(changeChecker);
+        medium.setOnCheckedChangeListener(changeChecker);
+        hard.setOnCheckedChangeListener(changeChecker);
+        extraHard.setOnCheckedChangeListener(changeChecker);
     }
-
-}
-
-/*
-ToggleButton toggle = (ToggleButton) findViewById(R.id.togglebutton);
-toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if (isChecked) {
-            // The toggle is enabled
-        } else {
-            // The toggle is disabled
-        }
-    }
-});
-
-
- */
-
-/*
-ToggleButton one;
-ToggleButton two;
-ToggleButton three;
-ToggleButton four;
-
-// Called when the activity is first created.
-@Override
-public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
-
-
-    one = (ToggleButton) findViewById(R.id.toggleButton1);
-    two = (ToggleButton) findViewById(R.id.toggleButton2);
-    three = (ToggleButton) findViewById(R.id.toggleButton3);
-    four = (ToggleButton) findViewById(R.id.toggleButton4);
-
-    one.setOnCheckedChangeListener(changeChecker);
-    two.setOnCheckedChangeListener(changeChecker);
-    three.setOnCheckedChangeListener(changeChecker);
-    four.setOnCheckedChangeListener(changeChecker);
-}
 
     OnCheckedChangeListener changeChecker = new OnCheckedChangeListener() {
 
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
             if (isChecked){
-                if (buttonView == one) {
-                    two.setChecked(false);
-                    three.setChecked(false);
-                    four.setChecked(false);
+                if (buttonView == easy) {
+                    medium.setChecked(false);
+                    hard.setChecked(false);
+                    extraHard.setChecked(false);
                 }
-                if (buttonView == two) {
-                    one.setChecked(false);
-                    three.setChecked(false);
-                    four.setChecked(false);
+                if (buttonView == medium) {
+                    easy.setChecked(false);
+                    hard.setChecked(false);
+                    extraHard.setChecked(false);
                 }
-                if (buttonView == three) {
-                    two.setChecked(false);
-                    one.setChecked(false);
-                    four.setChecked(false);
+                if (buttonView == hard) {
+                    medium.setChecked(false);
+                    easy.setChecked(false);
+                    extraHard.setChecked(false);
                 }
-                if (buttonView == four) {
-                    two.setChecked(false);
-                    three.setChecked(false);
-                    one.setChecked(false);
+                if (buttonView == extraHard) {
+                    medium.setChecked(false);
+                    hard.setChecked(false);
+                    easy.setChecked(false);
                 }
             }
         }
     };
 }
-
- */
